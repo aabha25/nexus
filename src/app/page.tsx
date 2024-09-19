5103a71f6d0888894fedea48b907c28c54893b37
aabha25 <aabha.jog@gmail.com>
2024-09-19 18:08:14 +0530

    basic table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>hello</p>
+    <table>
+      <tr>
+        <td>To Do<button>+</button></td>
+        <td>Preparing</td>
+        <td>Prepared</td>
+        <td>In Progress</td>
+        <td>Done</td>
+      </tr>
+       <Todorow/>
+      
+
+    </table>
   );
+}
+function Todorow() {
+  
+  
+ 
+  return(
+   <tr>
+    <td>data </td>
+    <td>data </td>
+    <td>data </td>
+    <td>data </td>
+    <td>data </td>
+  
+    
+   </tr> 
+   
+  ) ;
+ 
 }
