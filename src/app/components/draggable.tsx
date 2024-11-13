c83e59c3cdfe722ee85a58451e9215d961b6eb0f
aabha25 <aabha.jog@gmail.com>
2024-11-13 13:23:35 +0530

    post it inherits colour

--- src/app/components/draggable.tsx
+++ src/app/components/draggable.tsx
@@ -1,10 +1,12 @@
 import { useDraggable} from "@dnd-kit/core";
 import { CSS} from "@dnd-kit/utilities";
-import {FC} from "react";
+import {FC, useContext} from "react";
 import React from "react";
 import Image from "next/image";
 import blue from './blue.png';
-
+import plain from './plain.png';
+import { DialogContext } from "../context/Context";
+import { doesNotMatch } from "assert";
 
 
 interface IToDraggable {
@@ -16,37 +18,103 @@ const FruitDragable: FC<IToDraggable> = (props) => {
         id:props.items ,
         data: {title: props.items}
     });
+  const { taskList,preppedTask,doneTask,editTask,editpTask,editdTask} = useContext(DialogContext);
 
+    const deletePost = () =>{
+        const taskname=props.items;
+        console.log("clicked");
+        const index = taskList.tasks.findIndex((tab) => tab === taskname);
+       
+        if(index!=-1){
+        const nlist = [...taskList.tasks.slice(0, index), ...taskList.tasks.slice(index + 1)];
+        editTask({
+            tasks:nlist,
+        });
+       }
+       else {const index = preppedTask.findIndex((tab) => tab === taskname);
+        if(index!=-1){
+            const nlist = [...preppedTask.slice(0, index), ...preppedTask.slice(index + 1)];
+            editpTask(nlist);
+           }
+           else{const index = doneTask.findIndex((tab) => tab === taskname);
+            if(index!=-1){
+                const nlist = [...doneTask.slice(0, index), ...doneTask.slice(index + 1)];
+                editdTask(nlist);
+               }
+           }
+       }
+      
+    }
     return(
         // post it background
-        <div
+        <div id="bluepostit"
         ref={setNodeRef}
         style={{transform: CSS.Translate.toString(transform),
             backgroundColor:`var(--color3)`,  
             zIndex:30,
+    position: 'relative',
         }}
         {...attributes}
     {...listeners}        >
-        <div  id="bluepostit" style={{height: '100px',
-  width: '105px',
+        <div  style={{height: '100%',
 
-  backgroundColor: `var(--color3)`,
-  backgroundPosition: 'center',
-  margin: '10px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexWrap: 'wrap',
-  paddingLeft: '8px',
-  paddingRight: '5px',
-  textAlign: 'center',
-  fontFamily: `'Times New Roman', Times, serif`,
-            backgroundImage: `url(${blue})`,
-    backgroundSize: 'cover',
+            width:"100%",
+            position:"absolute",
+  top: "0px",
+  left: "0px",
+  
+  backgroundImage: `url(${plain.src})`,
+  backgroundSize: 'cover',
+  mixBlendMode: "luminosity",
+  backgroundColor:`var(--color4)`,
 
     
+    
    
-    }}>{props.items}</div>
+    }}>
+       <div style={{
+      width: '100%',
+      height: '100%',
+      top: "0px",
+      left: "0px",
+      position: "absolute",
+      backgroundImage: `url(${plain.src})`,
+      backgroundSize: 'cover',
+      mixBlendMode: "multiply",
+      backgroundColor:`var(--color4)`,
+  
+
+    }}>
+    
+    <div style={{
+      width: '100%',
+      height: '100%',
+      top: "0px",
+      left: "0px",
+      position: "absolute",
+      
+      backgroundSize: 'cover',
+     
+      backgroundColor:`var(--color4)`,
+      margin: '5px',
+      display: 'flex',
+        justifyContent:'center',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      paddingBottom:'15px',
+      paddingRight: '5px',
+      textAlign: 'center',
+      fontFamily: `'Times New Roman', Times, serif`,
+      color:"rgba(0,0,0,1)",
+      
+
+    }}>
+    <button onClick={deletePost}style={{position:'absolute',  paddingLeft:"8px",backgroundColor:`var(--color4)`,top:"0px",left:"0px",width:"15%",height:"15%"}}>x</button>
+    {props.items}
+    </div> 
+    </div> 
+  
+    </div>
     
         </div>
        
